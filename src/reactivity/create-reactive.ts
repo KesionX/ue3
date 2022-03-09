58a3a19887fe2aa2d0185fbbab1204da6c4ceae1
柯建芳 <673080885@qq.com>
2022-03-09 22:41:39 +0800

    fix: 格式化

--- src/reactivity/create-reactive.ts
+++ src/reactivity/create-reactive.ts
@@ -9,29 +9,29 @@ const RAW = "__RAW__";
 
 // 重写 'includes', 'indexOf', 'lastIndexOf'
 const arrayInstrumentations: Record<string, any> = {};
-['includes', 'indexOf', 'lastIndexOf'].forEach((method: string) => {
+["includes", "indexOf", "lastIndexOf"].forEach((method: string) => {
     const originMethod = (Array.prototype as any)[method];
-    arrayInstrumentations[method] = function (...args: any[]) {
+    arrayInstrumentations[method] = function(...args: any[]) {
         // 代理对象查找
-        let res = originMethod.apply(this, args)
+        let res = originMethod.apply(this, args);
         if (res === false) {
             // 原生查找
             res = originMethod.apply((this as any)[RAW], args);
         }
         return res;
-    }
+    };
 });
 
 let shouldTrack = true;
-['push', 'pop', 'shift', 'unshift', 'splice'].forEach((method: string) => {
+["push", "pop", "shift", "unshift", "splice"].forEach((method: string) => {
     const originMethod = (Array.prototype as any)[method];
-    arrayInstrumentations[method] = function (...args: any[]) {
+    arrayInstrumentations[method] = function(...args: any[]) {
         shouldTrack = false;
         const res = originMethod.apply(this, args);
         shouldTrack = true;
         return res;
     };
-})
+});
 
 // 防止生成多个新的代理
 const reactiveMap = new Map();
@@ -41,7 +41,6 @@ export function createReactive<T extends Record<PropertyKey, any>>(
     option?: IReactiveOption,
     iterateKey?: symbol
 ) {
-
     if (reactiveMap.has(data)) {
         return reactiveMap.get(data);
     }
@@ -52,11 +51,17 @@ export function createReactive<T extends Record<PropertyKey, any>>(
             if (key === RAW) {
                 return target;
             }
-            if (Array.isArray(target) && arrayInstrumentations.hasOwnProperty(key)) {
+            if (
+                Array.isArray(target) &&
+                arrayInstrumentations.hasOwnProperty(key)
+            ) {
                 return Reflect.get(arrayInstrumentations, key, receiver);
             }
 
-            !option?.readonly && typeof key !== 'symbol' && shouldTrack && track(target, key);
+            !option?.readonly &&
+                typeof key !== "symbol" &&
+                shouldTrack &&
+                track(target, key);
 
             const res = Reflect.get(target, key, receiver);
             if (option?.isShallow) {
@@ -98,7 +103,8 @@ export function createReactive<T extends Record<PropertyKey, any>>(
             return Reflect.has(target, key);
         },
         ownKeys(target: T) {
-            shouldTrack && track(target, Array.isArray(target) ? 'length' : ITERATE_KEY);
+            shouldTrack &&
+                track(target, Array.isArray(target) ? "length" : ITERATE_KEY);
             return Reflect.ownKeys(target);
         },
         deleteProperty(target: T, key: PropertyKey) {
