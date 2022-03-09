55d7882cdc5811e6053add8009e6342f2b0bca9b
柯建芳 <673080885@qq.com>
2022-03-09 22:42:40 +0800

    feat: export watch

--- src/reactivity/index.ts
+++ src/reactivity/index.ts
@@ -4,6 +4,7 @@ export { shallowReactive } from "./shallow-reactive.js";
 export { readonly } from "./readonly.js";
 export { shallowReadonly } from "./shallow-readonly.js";
 export { effect } from "./effect.js";
+export { watch } from "./watch.js";
 
 export function ref(data: any) {
     return createReactive(data);
