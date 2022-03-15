f5066b4e085edcb386a38417d0a00f8e46cd5a30
柯建芳 <673080885@qq.com>
2022-03-15 22:58:14 +0800

    fix: 注释

--- src/types/render.ts
+++ src/types/render.ts
@@ -1,6 +1,18 @@
 export interface RendererAdapter {
+    /**
+     * 创建元素
+     */
     createElement: (tag: string) => HTMLElement;
+    /**
+     * 插入
+     */
     insert: (el: HTMLElement, parent: HTMLElement, anchor?: any) => void;
+    /**
+     * 设置元素text
+     */
     setElementText: (el: HTMLElement, text: string) => void;
+    /**
+     * 设置属性值
+     */
     patchProps: (el: HTMLElement, key: string, prevValue: any, nextValue: any) => void; 
 }
