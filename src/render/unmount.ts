388b0aa99f4cecd2b770ad9f0da58d30552b45b4
柯建芳 <673080885@qq.com>
2022-03-16 23:00:37 +0800

    fragment类型移除

--- src/render/unmount.ts
+++ src/render/unmount.ts
@@ -1,9 +1,14 @@
+import { Fragment } from "../types";
 import { VNode } from "../types/vnode";
 
 export function unmount(vnode: VNode | null) {
     if (!vnode) {
         return;
     }
+    vnode.type === Fragment &&
+        (vnode.children as VNode[]).forEach(child => {
+            unmount(child);
+        });
     const el = vnode.el;
     const parent = el.parentNode;
     parent && parent.removeChild(el);
