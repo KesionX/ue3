9cbd577eec90e7c348a329c86dfcd3ebe4e47986
柯建芳 <673080885@qq.com>
2022-03-16 22:59:51 +0800

    无用代码

--- src/render/render.ts
+++ src/render/render.ts
@@ -1,50 +1,50 @@
 import { VNode } from "../types/vnode";
 
-export function render(vnode: VNode, container: HTMLElement): HTMLElement {
-    if (typeof vnode.type === "string" || vnode.type instanceof HTMLElement) {
-        return mountElement(vnode, container);
-    } else {
-        return mountComponent(vnode, container);
-    }
-}
+// export function render(vnode: VNode, container: HTMLElement): HTMLElement {
+    // if (typeof vnode.type === "string" || vnode.type instanceof HTMLElement) {
+    //     return mountElement(vnode, container);
+    // } else {
+    //     return mountComponent(vnode, container);
+    // }
+// }
 
-function mountElement(vnode: VNode, container: HTMLElement) {
-    let el = vnode.type;
-    if (!(el instanceof HTMLElement)) {
-        el = document.createElement(vnode.type as string);
-    }
+// function mountElement(vnode: VNode, container: HTMLElement) {
+//     let el = vnode.type;
+//     if (!(el instanceof HTMLElement)) {
+//         el = document.createElement(vnode.type as string);
+//     }
 
-    // handle props
-    for (const key in vnode.props) {
-        // emit
-        if (/^on/.test(key)) {
-            continue;
-        }
-        const attrValue = vnode.props[key];
-        // juest attrvalue string
-        if (typeof attrValue === "string") {
-            el.setAttribute(key, attrValue);
-        }
-        // TODO
-    }
-    // handle children
-    if (typeof vnode.children === "string") {
-        el.innerText = vnode.children;
-    } else {
-        vnode.children &&
-            vnode.children.forEach(child => {
-                (el as HTMLElement).appendChild(
-                    render(child, el as HTMLElement)
-                );
-            });
-    }
+//     // handle props
+//     for (const key in vnode.props) {
+//         // emit
+//         if (/^on/.test(key)) {
+//             continue;
+//         }
+//         const attrValue = vnode.props[key];
+//         // juest attrvalue string
+//         if (typeof attrValue === "string") {
+//             el.setAttribute(key, attrValue);
+//         }
+//         // TODO
+//     }
+//     // handle children
+//     if (typeof vnode.children === "string") {
+//         el.innerText = vnode.children;
+//     } else {
+//         vnode.children &&
+//             vnode.children.forEach(child => {
+//                 (el as HTMLElement).appendChild(
+//                     render(child, el as HTMLElement)
+//                 );
+//             });
+//     }
 
-    container.appendChild(el);
-    return el;
-}
+//     container.appendChild(el);
+//     return el;
+// }
 
-function mountComponent(vnode: VNode, container: HTMLElement) {
-    // @ts-ignore TODO
-    const subtree = (vnode.type as ComponentFunction)();
-    return render(subtree, container);
-}
+// function mountComponent(vnode: VNode, container: HTMLElement) {
+//     // @ts-ignore TODO
+//     const subtree = (vnode.type as ComponentFunction)();
+//     return render(subtree, container);
+// }
