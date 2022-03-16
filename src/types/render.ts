9f2c9794f96fe02e0c7676bddfbafbe29442b7ad
柯建芳 <673080885@qq.com>
2022-03-16 23:01:18 +0800

    适配器新增方法

--- src/types/render.ts
+++ src/types/render.ts
@@ -6,7 +6,7 @@ export interface RendererAdapter {
     /**
      * 插入
      */
-    insert: (el: HTMLElement, parent: HTMLElement, anchor?: any) => void;
+    insert: (el: HTMLElement | Text, parent: HTMLElement, anchor?: any) => void;
     /**
      * 设置元素text
      */
@@ -14,5 +14,13 @@ export interface RendererAdapter {
     /**
      * 设置属性值
      */
-    patchProps: (el: HTMLElement, key: string, prevValue: any, nextValue: any) => void; 
+    patchProps: (el: HTMLElement, key: string, prevValue: any, nextValue: any) => void;
+    /**
+     * 创建text
+     */
+    createText: (text: string) => Text;
+    /**
+     * 配置text值
+     */
+    setText: (el: HTMLElement, text: string) => void;
 }
