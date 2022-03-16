15e3e251633d0b7d7c8ad859763e0d605eb89681
柯建芳 <673080885@qq.com>
2022-03-16 23:00:49 +0800

    特殊类型

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,5 +1,8 @@
 import { VNode } from "./vnode";
 
+export const VTypeText = Symbol();
+export const VTypeComment = Symbol();
+export const Fragment = Symbol();
 export interface VHTMLElement extends HTMLElement {
     _vnode: VNode | null;
     _vei: Record<string, any>;
