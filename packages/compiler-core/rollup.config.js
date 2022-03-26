8743153d8573301f059e8fe9c8ebf1d965755157
柯建芳 <673080885@qq.com>
2022-03-27 00:51:51 +0800

    feat: 编译

---
+++ packages/compiler-core/rollup.config.js
@@ -0,0 +1,26 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
+    input: "./src/index.ts",
+    output: [
+        {
+            name: "es",
+            dir: "dist",
+            format: "esm",
+            preserveModules: true,
+            preserveModulesRoot: "./src"
+        }
+    ],
+    plugins: [
+        typescript({ tsconfig: "./tsconfig.json" }),
+        nodeResolve(),
+        commonjs()
+    ]
+};
+
+export default config;
